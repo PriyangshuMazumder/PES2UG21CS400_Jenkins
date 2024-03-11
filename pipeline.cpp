5ab79d344e2dc32e603ae8ef1018da3acf7f5595
Priyangshu Mazumder <97266247+PriyangshuMazumder@users.noreply.github.com>
2024-03-11 15:23:40 +0530

    Create pipeline.cpp

---
+++ pipeline.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, world!" << std::endl;
+  std::cout << "FROM PES2UG21CS400" << std::endl;
+  std::cout << "This is using Pipeline" << std::endl;
+  std::cout << "Bye Bye ..." << std::endl;
+  return 0;
+}
